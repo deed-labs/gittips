c05d174bb5c1a5e546f327486610058268bd18a1
AlexeySadkovich <sadkovich01@mail.ru>
2023-03-11 09:25:58 +0300

    fix: update client hooks

--- web/src/hooks.client.ts
+++ web/src/hooks.client.ts
@@ -1,9 +1,13 @@
 import { TON } from './lib/stores';
 import { MyTonWallet, OpenMask, TonKeeper, TonSafe } from './lib/pkg/wallet/ton';
 
-await TON.init({
-	TonKeeper: new TonKeeper(),
-	OpenMask: new OpenMask(),
-	MyTonWallet: new MyTonWallet(),
-	TonSafe: new TonSafe()
-});
+// Wrap async calls with async function that executes itself to decrease the level of await.
+// See https://github.com/vitejs/vite/issues/6985
+(async () => {
+	await TON.init({
+		TonKeeper: new TonKeeper(),
+		OpenMask: new OpenMask(),
+		MyTonWallet: new MyTonWallet(),
+		TonSafe: new TonSafe()
+	});
+})();
